26e44195bbbfa80f3293ae433d185c307f7194e4
kurt <rond9275@gmail.com>
2025-09-15 19:25:28 +0800

    completed the mobile order tile

--- lib/presentation/screens/cashier/orders/cashier_mobile_order_screen.dart
+++ lib/presentation/screens/cashier/orders/cashier_mobile_order_screen.dart
@@ -13,9 +13,11 @@ class CashierMobileOrderScreen extends StatefulWidget {
 }
 
 class _CashierMobileOrderScreenState extends State<CashierMobileOrderScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -31,27 +33,70 @@ class _CashierMobileOrderScreenState extends State<CashierMobileOrderScreen> {
               ],
             ),
           ),
-          Text(
-            'Menu',
-            style: GoogleFonts.roboto(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Text(
+              'Menu',
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
             ),
           ),
           Expanded(
             child: GridView.count(
+              controller:ScrollController(initialScrollOffset: 18),
               crossAxisCount: 2,
               childAspectRatio: 2 / 2.2,
               children: [
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
-                MobileOrderTile(),
+                MobileOrderTile(
+                  name: "Caramel Coffee Jelly Frappuccino",
+                  price: 175,
+                  stock: 23,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Mocha Frappuccino",
+                  price: 160,
+                  stock: 15,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Java Chip Frappuccino",
+                  price: 180,
+                  stock: 10,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Strawberries & Cream Frappuccino",
+                  price: 185,
+                  stock: 8,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Caramel Macchiato",
+                  price: 170,
+                  stock: 12,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Caffè Americano",
+                  price: 140,
+                  stock: 20,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Chocolate Muffin",
+                  price: 95,
+                  stock: 30,
+                  imagePath: "",
+                ),
+                MobileOrderTile(
+                  name: "Classic Cheesecake",
+                  price: 215,
+                  stock: 5,
+                  imagePath: "",
+                ),
               ],
             ),
           ),
--- lib/presentation/widgets/mobile_order_tile.dart
+++ lib/presentation/widgets/mobile_order_tile.dart
@@ -1,22 +1,81 @@
 import 'package:flutter/material.dart';
 
-class MobileOrderTile extends StatefulWidget {
-  const MobileOrderTile({super.key});
+class MobileOrderTile extends StatelessWidget {
+  final String name;
+  final double price;
+  final int stock;
+  final String imagePath;
 
-  @override
-  State<MobileOrderTile> createState() => _MobileOrderTileState();
-}
+  const MobileOrderTile({
+    super.key,
+    required this.name,
+    required this.price,
+    required this.stock,
+    required this.imagePath,
+  });
 
-class _MobileOrderTileState extends State<MobileOrderTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120,
-      width: 100,
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(20)
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(2, 2),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+            child: Image.asset(
+              imagePath,
+              height: 120,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\₱$price",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      stock > 0 ? "Stock: $stock" : "Out of Stock",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: stock > 0 ? Colors.grey : Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
